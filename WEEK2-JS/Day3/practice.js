3db354a4ec6690b5c30c03838d41daccb5881a52
Akash <akkyjat601@gmail.com>
2024-02-20 12:02:18 +0530

    addition sum

--- WEEK2-JS/Day3/practice.js
+++ WEEK2-JS/Day3/practice.js
@@ -1,6 +1,59 @@
-console.log(typeof new Date().getDay());
+const readline = require("readline");
 
-switch (new Date().getDay()) {
-    case 1:
-        console.log(1);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let output = 0;
+
+function processData(data) {
+    data = parseInt(data, 10);
+    if (data >= 0) {
+        output += data;
+    } else {
+        console.log(output);
+        rl.close();
+    }
 }
+function getInput() {
+    rl.question("Please enter a number:", (input) => {
+        processData(input);
+        if (input >= 0) {
+            getInput();
+        }
+    });
+}
+getInput();
+
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// let total = 0;
+
+// function processInput(input) {
+//     const num = parseInt(input);
+//     if (isNaN(num)) {
+//         console.log("Please enter a valid number.");
+//     } else if (num >= 0) {
+//         total += num;
+//     } else {
+//         console.log(`Total: ${total}`);
+//         rl.close();
+//     }
+// }
+
+// function getInput() {
+//     rl.question("Enter a number: ", (input) => {
+//         processInput(input);
+//         if (parseInt(input) >= 0) {
+//             getInput(); // Continue reading input
+//         }
+//     });
+// }
+
+// getInput(); // Start reading input

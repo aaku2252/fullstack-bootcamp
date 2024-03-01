d93f515a95e79af36c4ced17c30fb909f124d21c
Akash <akkyjat601@gmail.com>
2024-03-01 13:27:46 +0530

    php

---
+++ WEEK4-PHP,LARAVEL/index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <title>My first  App</title>
+</head>
+
+<body>
+
+<h1>Welcome to my first php app.</h1>
+
+<?php
+
+// ---------------------------->
+
+//> variable 
+// $a = 'new val';
+// $b = 123;
+// $c= 10.5;
+// $d='123';
+
+
+//> datatype and showing data
+
+// $e= (int)$c; 
+// echo var_dump($a.$b);
+// echo '<br/>';
+// echo $a.$b;
+// $bool= true;
+// echo var_dump($bool);
+
+//>  operators
+
+// same as javscript
+// ==,===,!=,>=,<=,!=
+// +,-,*,/,%,**
+
+
+
+//> Increment /decrement operator
+// ++$a:first increse value then assign
+// $a++:first assign and then increse
+//--$a
+//$a++
+
+//> Logical operator
+// and
+// or
+// not : !
+// xor - when both conditions are true then it will return false.If any of the conditon is false then it will return true.
+
+
+//> Conditional operator 
+// if else statement
+
+// $total_bill = 500;
+// if($total_bill>=5000){
+//     $discount= $total_bill*10/100;
+//     $final_bill = $total_bill-$discount;
+//     echo 'Final Bill : '.$final_bill;
+// }else if($total_bill>1000){
+//     $discount= $total_bill*5/100;
+//     $final_bill = $total_bill-$discount;
+//     echo 'Final Bill : '.$final_bill;
+
+// }else{
+    
+//     echo 'Final Bill : '.$total_bill;
+// }
+
+
+//>switch case
+
+
+
+
+
+
+
+
+// ---------------------------->
+?>
+
+</body>
+
+</html>

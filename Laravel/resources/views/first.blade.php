e72a60e40b50943c4933b83b5956937195d90a4b
Akash <akkyjat601@gmail.com>
2024-03-06 13:21:37 +0530

    Laravel

---
+++ Laravel/resources/views/first.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="" method="post">
+            Name:<input type="text" name="name"><br/>
+            Phone:<input type = 'text' name ="phone"><br/>
+            Password:<input type="text" name="password"><br/>
+            <input type="Submit" name="submit" >
+        </form>
+    </body>
+</html>
